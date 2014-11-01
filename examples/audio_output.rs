73388cc09b3cfe24c55742ae73dd8cf84a316e60
Allen Welkie <allen@localhost.localdomain>
2014-11-01 15:22:57 -0400

    Changed from pulse-audio to portaudio for audio output example

--- examples/audio_output.rs
+++ examples/audio_output.rs
@@ -1,23 +1,30 @@
-extern crate pasimple;
+extern crate portaudio;
+
+use portaudio::{pa, types};
 
 /// Plays constant A4
+#[allow(unreachable_code)]
 fn main() {
-    let sample_rate = 32000;
+    let sample_rate = 32000.0;
     let sine_freq = 440u;
+    let buffer_size = 1024;
 
-    let (tx, rx) = channel();
-    spawn(proc() {
-        pasimple::pulse_sink(rx, sample_rate);
-    });
+    pa::initialize();
+
+    let mut stream : pa::PaStream<f32> = pa::PaStream::new(types::PaFloat32);
+    stream.open_default(sample_rate, 0, 0, 1, types::PaFloat32);
+    stream.start();
 
     let mut phase = 0f32;
     let radians_per_sample = (sine_freq as f32) / (sample_rate as f32) * Float::two_pi();
-    let samples_per_chunk = 10000;
     loop {
-        let samples: Vec<f32> = Vec::from_fn(samples_per_chunk, |_| {
+        let samples: Vec<f32> = Vec::from_fn(buffer_size as uint, |_| {
             phase += radians_per_sample;
             phase.sin()
         });
-        tx.send(samples);
+        stream.write(samples, buffer_size);
     }
+
+    stream.close();
+    pa::terminate();
 }
