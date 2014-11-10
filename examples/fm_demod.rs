2793c944a6edb0775b124ad4f3c93aca65876fb6
Allen Welkie <allen@localhost.localdomain>
2014-11-09 20:42:51 -0500

    Corrected the low-pass filter. Audio now sounds fine.

--- examples/fm_demod.rs
+++ examples/fm_demod.rs
@@ -3,8 +3,10 @@
 
 extern crate rustradio;
 extern crate num;
+extern crate portaudio;
 
 use num::Complex;
+use portaudio::{pa, types};
 
 use rustradio::file;
 use rustradio::blocks::modem::PhaseDiffs;
@@ -16,33 +18,46 @@ use rustradio::blocks::filter::{RationalResampler,
 
 fn main() {
     let input_filename = Path::new("/home/allen/repos/gr-tutorial/examples/tutorial6/fm_101.8MHz_1Msps.cfile");
-    let output_filename = Path::new("./output.bin");
+    let audio_sample_rate = 50000.0;
+    let pa_buffer_size = 2048;
 
-    let lp_taps = low_pass_filter_taps(HammingWindow, 100e3 / 4e6, NumTaps(100));
+    /*
+        Set up processing blocks
+    */
+    let lp_taps = low_pass_filter_taps(HammingWindow, 100e3 / 2e6, NumTaps(100));
     // Until we have multidispatch, the samples and the taps need to be the same type
     let lp_taps: Vec<Complex<f32>> = lp_taps.into_iter()
                                             .map(|x| Complex{re: x, im: 0.0}).collect();
-    let Resampler1 = RationalResampler{up: 1, down: 2, taps: lp_taps.as_slice()};
+    let rf_resampler = RationalResampler{up: 1, down: 2, taps: lp_taps.as_slice()};
 
     let lp_taps = low_pass_filter_taps(HammingWindow, 1.0/20.0, NumTaps(100));
-    let Resampler2 = RationalResampler{up: 1, down: 10, taps: lp_taps.as_slice()};
+    let audio_resampler = RationalResampler{up: 1, down: 10, taps: lp_taps.as_slice()};
 
-    let mut s_count = 0u;
-    let mut r1_count = 0u;
-    let mut pd_count = 0u;
-    let mut r2_count = 0u;
+    /*
+        Connect blocks
+    */
+    let source = file::read_interleaved_float(&input_filename);
+    connect!(downsampled <- rf_resampler (source));
+    connect!(diffs <- PhaseDiffs (downsampled));
+    connect!(mut audio <- audio_resampler (diffs));
 
-    {
-        let source = file::read_interleaved_float(&input_filename);
-        let source = source.inspect(|_| s_count+=1);
-        connect!(downsampled <- Resampler1 (source));
-        let downsampled = downsampled.inspect(|_| r1_count+=1);
-        connect!(diffs <- PhaseDiffs (downsampled));
-        let diffs = diffs.inspect(|_| pd_count+=1);
-        connect!(audio <- Resampler2 (diffs));
-        let audio = audio.inspect(|_| r2_count+=1);
-        file::write_float(&output_filename, audio);
+    /*
+        Set up PortAudio output
+    */
+    pa::initialize();
+    let mut stream : pa::PaStream<f32> = pa::PaStream::new(types::PaFloat32);
+    stream.open_default(audio_sample_rate, 0, 0, 1, types::PaFloat32);
+    stream.start();
+
+    let mut samples: Vec<f32> = Vec::with_capacity(pa_buffer_size);
+    for sample in audio {
+        samples.push(sample);
+        if samples.len() == pa_buffer_size {
+            stream.write(samples.clone(), pa_buffer_size as u32);
+            samples.clear();
+        }
     }
 
-    println!("{}, {}, {}, {}", s_count, r1_count, pd_count, r2_count);
+    stream.close();
+    pa::terminate();
 }
