9cc9ba108f86faf7bcb760d7bc73f3168c70bb7a
Allen Welkie <allen.welkie@link-labs.com>
2014-11-11 15:28:51 -0500

    File IO now works on any Copy type

--- src/file.rs
+++ src/file.rs
@@ -1,42 +1,64 @@
 extern crate num;
 
-use std::io::{File, IoResult, Reader, BufferedReader, BufferedWriter, Open, Write};
-use self::num::complex::{Complex};
+use std::io::{File, Reader, BufferedReader, BufferedWriter, Open, Write};
+use std::mem;
+use std::raw;
 
-pub struct ReaderIterator<'r, Buff: Reader + 'r, T> {
+pub struct ReaderIterator<Buff: Reader, T: Copy> {
     buffer: Buff,
-    f: |b: &mut Buff|: 'r -> IoResult<T>,
 }
 
-/// Iterates over any Reader object, stopping whenever any error is hit.
-impl<'r, Buff: Reader + 'r, T> Iterator<T> for ReaderIterator<'r, Buff, T> {
+impl <Buff: Reader, T: Copy> Iterator<T> for ReaderIterator<Buff, T> {
     fn next(&mut self) -> Option<T> {
-        (self.f)(&mut self.buffer).ok()
-    }
-}
-
-pub fn read_interleaved_int16<'r>(filename: &Path) -> ReaderIterator<'r, BufferedReader<File>, Complex<i16>> {
-    let file = File::open(filename).unwrap(); // FIXME
-    let reader = BufferedReader::new(file);
-    ReaderIterator {
-        buffer: reader,
-        // TODO How can we make the endianess depend on the current machine?
-        f: |b: &mut BufferedReader<File>| match (b.read_le_i16(), b.read_le_i16()) {
-            (Ok(re), Ok(im)) => Ok(Complex{ re: re, im: im }),
-            (Err(err), _) => Err(err),
-            (_, Err(err)) => Err(err),
+        match self.buffer.read_exact(mem::size_of::<T>()) {
+            Err(_) => None,
+            Ok(bytes) => unsafe {
+                let ptr: &u8 = mem::transmute(bytes.as_ptr());
+                Some(mem::transmute_copy(ptr))
+            }
         }
     }
 }
 
-//TODO how can we make this agnostic to the Complex type?
-pub fn write_interleaved_int16<'r, I>(filename: &Path, mut input: I)
-where I: Iterator<Complex<i16>> {
+pub fn read_stream<T: Copy>(filename: &Path) -> ReaderIterator<BufferedReader<File>, T> {
+    let file = File::open(filename).unwrap(); // FIXME
+    let reader = BufferedReader::new(file);
+    ReaderIterator {
+        buffer: reader
+    }
+}
+
+pub fn write_stream<'r, T, I>(filename: &Path, mut input: I)
+where T: Copy, I: Iterator<T> {
     let file = File::open_mode(filename, Open, Write);
     let mut writer = BufferedWriter::new(file);
-    for Complex{re: i, im: q} in input {
-        writer.write_le_i16(i)
-            .and_then(|()| writer.write_le_i16(q))
-            .unwrap_or_else(break);
+    for item in input {
+        let slice: &[u8] = unsafe {
+            mem::transmute(raw::Slice {
+                data: &item as *const _ as *const u8,
+                len: mem::size_of::<T>()
+            })
+        };
+        if writer.write(slice).is_err() {
+            break;
+        }
     }
+}
+
+#[test]
+fn write_then_read() {
+    use std::io::TempDir;
+    use self::num::complex::{Complex};
+
+    let source = vec![Complex{re: 0f32, im: 3f32},
+                      Complex{re: 1f32, im: 2f32},
+                      Complex{re: 2f32, im: 1f32},
+                      Complex{re: 3f32, im: 0f32}];
+    let temp_dir = TempDir::new("RustRadio").unwrap();
+    let mut temp_file = temp_dir.path().clone();
+    temp_file.set_filename("test_file");
+
+    write_stream(&temp_file, source.iter().map(|&x| x));
+    let result: Vec<Complex<f32>> = read_stream(&temp_file).collect();
+    assert_eq!(source, result);
 }
